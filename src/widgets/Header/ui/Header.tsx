ce186cc68a02fa61496c4a92a7372dc090ecc1ba
Daniil Sintsov <daniilpanfilov519@gmail.com>
2024-03-10 00:28:03 +0300

    completed sidemenu

--- src/widgets/Header/ui/Header.tsx
+++ src/widgets/Header/ui/Header.tsx
@@ -8,10 +8,10 @@ import Image from 'next/image';
 import { useState } from 'react';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
+import CloseIcon from '@mui/icons-material/Close';
 import Drawer from '@mui/material/Drawer';
 import Navlink from '@/src/shared/Navlink';
+import Link from 'next/link';
 
 export default function Header() {
 	const [mobileOpen, setMobileOpen] = useState(false);
@@ -20,20 +20,47 @@ export default function Header() {
 		setMobileOpen(prevState => !prevState);
 	};
 
-	const drawerWidth = 240;
-	const container = window !== undefined ? () => window.document.body : undefined;
+	const drawerWidth = 320;
+
+	const Logo = () => (
+		<Link
+			href="/"
+			className={styles.logoContainer}>
+			<div className={styles.logo}>
+				<Image
+					src="/img/logo.png"
+					width={50}
+					height={50}
+					alt="youreader"
+				/>
+			</div>
+			<p className={styles.text}>YouReader</p>
+		</Link>
+	);
 
 	const drawer = (
-		<Box
-			onClick={handleDrawerToggle}
-			sx={{ textAlign: 'center' }}>
-			<List>
-				<ListItem>
-					<Navlink href="/">Мои книги</Navlink>
-					<Navlink href="/">Папки</Navlink>
-				</ListItem>
-			</List>
-		</Box>
+		<div>
+			<div className={styles.header}>
+				<Container sx={{ height: '100%' }}>
+					<ToolBar
+						sx={{
+							height: '100%',
+							minHeight: { xs: 'unset', md: 'unset' },
+							justifyContent: 'space-between',
+						}}
+						disableGutters>
+						<Logo />
+						<IconButton
+							color="inherit"
+							aria-label="open drawer"
+							onClick={handleDrawerToggle}
+							sx={{ display: { xs: 'flex', md: 'none' } }}>
+							<CloseIcon />
+						</IconButton>
+					</ToolBar>
+				</Container>
+			</div>
+		</div>
 	);
 
 	return (
@@ -46,22 +73,14 @@ export default function Header() {
 				<ToolBar
 					sx={{
 						height: '100%',
-						columnGap: { xs: 0, md: 3 },
+						columnGap: { xs: 0, md: 4 },
 						minHeight: { xs: 'unset', md: 'unset' },
 						justifyContent: { xs: 'space-between', md: 'flex-start' },
 					}}
 					disableGutters>
-					<div className={styles.logoWrapper}>
-						<Image
-							src="/img/logo.png"
-							width={50}
-							height={50}
-							alt="youreader"
-						/>
-					</div>
+					<Logo />
 					<Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
 						<Navlink href="/">Мои книги</Navlink>
-						<Navlink href="/">Папки</Navlink>
 					</Box>
 					<IconButton
 						color="inherit"
@@ -74,7 +93,7 @@ export default function Header() {
 			</Container>
 			<nav>
 				<Drawer
-					container={container}
+					container={window.document.body}
 					variant="temporary"
 					open={mobileOpen}
 					onClose={handleDrawerToggle}
