c55d55a3ca5ea73ff68dc8f6651068f379ffcd2b
Daniil Sintsov <daniilpanfilov519@gmail.com>
2024-05-12 14:51:33 +0300

    added suspense to avoid errors in production

--- app/(with-header)/login/page.tsx
+++ app/(with-header)/login/page.tsx
@@ -1,5 +1,10 @@
 import Login from '@/src/pages/Login';
+import { Suspense } from 'react';
 
 export default function LoginPage() {
-	return <Login />;
+	return (
+		<Suspense>
+			<Login />
+		</Suspense>
+	);
 }
